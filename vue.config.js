5addcda69a59d5986e089ade4213e1ac203f2b68
Duarte Thiago <duartetbatista@gmail.com>
2022-05-26 22:25:40 -0300

    fix[]: removed vuetify-loader and webpack-plugin-vuetify

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/webpack-plugin-vuetify/tree/next/packages/webpack-plugin-vuetify
 		}
   }
 })
